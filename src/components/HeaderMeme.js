61a9b3a25a4644a3576e74483c9598a5820b007c
Yosefha4 <73281960+Yosefha4@users.noreply.github.com>
2022-08-11 19:37:12 +0300

    Update HeaderMeme.js

--- src/components/HeaderMeme.js
+++ src/components/HeaderMeme.js
@@ -9,7 +9,7 @@ export default function HeaderMeme() {
       className='header-image'
       />
       <h2 className="header-title"> Meme Generator </h2>
-      <h4 className="header-project">React Course - Project 3</h4>
+      <h4 className="header-project">React js </h4>
     </header>
   );
 }
